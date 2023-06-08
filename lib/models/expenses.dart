db9c3b4997d4b567007028a8db55bd0649b9ad47
Matthew McDevitt <mmcdevitt1997@gmail.com>
2023-06-08 12:13:57 -0500

    data created

--- lib/models/expenses.dart
+++ lib/models/expenses.dart
@@ -10,12 +10,13 @@ work,
 }
 
 class Expense {
-  Expense({required this.title, required this.amount, required this.date})
+  Expense({required this.title, required this.amount, required this.date, required category})
       : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 
 }
