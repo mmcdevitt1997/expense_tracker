1e29873ba16ecf4da11bfcf90e80bf5a0253cd93
Matthew McDevitt <mmcdevitt1997@gmail.com>
2023-06-10 12:02:18 -0500

    scaffold add

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,13 +30,15 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('The chart'),
-        Expanded(
-         child: ExpensesList(expenses: _registeredExpenses,),
-        )
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+           child: ExpensesList(expenses: _registeredExpenses,),
+          )
+        ],
+      ),
     );
   }
 }
