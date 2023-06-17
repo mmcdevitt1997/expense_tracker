a7e76daad34fac8ce4add91bc568fb39e185741e
Matthew McDevitt <mmcdevitt1997@gmail.com>
2023-06-17 12:55:44 -0500

    adding complete

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -31,10 +31,17 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
         context: context,
         builder: (ctx) {
-          return const NewExpense();
+          return NewExpense(onAddExpense: _addExpense);
         });
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
   }
 
   @override
